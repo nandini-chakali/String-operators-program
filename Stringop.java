e38fe1370d3c7e576fe6067073300eb67d4bf20e
nandini-chakali <chakalinandini31@gmail.com>
2025-01-28 17:14:50 +0530

    Add files via upload

---
+++ Stringop.java
@@ -0,0 +1,10 @@
+public class Stringop{
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Length: " + str.length());
+        System.out.println("Replaced: " + str.replace('e', 'a'));
+        System.out.println("Substring: " + str.substring(1, 4));
+    }
+}
+
